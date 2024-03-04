fd5b6677c9a6a28f1c3d7f20c41b405ff160a409
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-03-04 23:22:02 +0530

    online status hook added

---
+++ src/hooks/useOnlineStatus.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+const useOnlineStatus = () => {
+    const [status, setStatus] = useState(true);
+
+    useEffect(() => {
+        const handleOnline = () => {
+            setStatus(true);
+        }
+
+        const handleOffline = () => {
+            setStatus(false);
+        }
+
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+
+        return () => {
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+        }
+    }, []);
+
+    return status;
+}
+
+export default useOnlineStatus;
